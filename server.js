68b5023b91b6b09741c7ecc146b96b3f3971e33a
Adam Svetec <adamsvetec@gmail.com>
2016-12-09 16:04:44 +0000

    add bar lean instead of numeric lean

--- server.js
+++ server.js
@@ -68,32 +68,40 @@ app.get('/directory', function (req, res) {
     connection.query(queryString, function(err_query, data){
 	var arr = []
 	for(var i = 0; i < data.length; i++)
-	    arr.push(data[i].Orgname)
+	    arr.push(data[i])
 	console.log("First val: " + arr[42])
 	console.log(arr)
 	res.render(
 	    __dirname + '/templates/directory.jade',
-	    {list: arr})
+	    {list: data})
     })
 })
 		  
 app.post('/company', function (req, res) {
     var userSearch = req.body.comp_name
-    var queryString = 'SELECT * from  organization, org_score, issue  where organization.Orgname like \'' + userSearch + "' AND organization.Id=org_score.org_id AND issue.id=org_score.issue_id";
+    var queryString = "SELECT * from  organization, org_score, issue  where organization.Orgname like '%" + userSearch + "%' AND organization.Id=org_score.org_id AND issue.id=org_score.issue_id ORDER BY org_score.org_id, issue.Id";
     console.log(queryString)
     connection.query(queryString, function(err_query, data) {
 	//console.log(data)
 	if(typeof data[0] != 'undefined' && data){
 	    var desc  = []
+	    var scores = [10]
 	    for(var i = 0; i < 10; i++)
 	    {
+		scores[i] = [2]
+		scores[i][0] = 0
+		scores[i][1] = 0
 		if(data[i].score < 0){
 		    console.log("NEGATIVE")
 		    desc.push(data[i].issue_negative_desc)
+		    scores[i][0]= Math.floor(data[i].score * -100)
+		    console.log(scores[i][0])
 		}
 		else if(data[i].score > 0){
 		    console.log("POSITIVE")
 		    desc.push(data[i].issue_positive_desc)
+		    scores[i][1]=Math.floor(data[i].score * 100)
+		    console.log(scores[i][1])
 		}
 		else{
 		    desc.push("No calculated lean.")
@@ -102,12 +110,19 @@ app.post('/company', function (req, res) {
 	    }
 	    res.render(
 		__dirname + '/templates/company.jade',
-		{company: data[0].Orgname, total_amount: data[0].total_amount, total_contr: data[0].total_contr,
-		 abortion: data[0].score, abortion_lean: desc[0],  financial_regulation: data[1].score, financial_regulation_lean: desc[1],
-		 STI: data[2].score, STI_lean: desc[2], immigration: data[3].score, immigration_lean: desc[3],
-		 foreign_trade: data[4].score, foreign_trade_lean: desc[4], healthcare_score: data[5].score, healthcare_lean: desc[5],
-		gun_control_score: data[6].score, gun_control_lean: desc[6], taxation_score: data[7].score, taxation_lean: desc[7],
-		 labor_score: data[8].score, labor_lean: desc[8], environment_score: data[9].score, environment_lean: desc[9]})
+		{company: data[0].Orgname, total_amount: data[0].total_amount.toLocaleString('en-US'), total_contr: data[0].total_contr.toLocaleString('en-US'),
+		 abortion: data[0].score, abortion_lean: desc[0], abortion_lean_l: scores[0][0], abortion_lean_r: scores[0][1],
+		 financial_regulation: data[1].score, financial_regulation_lean: desc[1], fin_lean_l: scores[1][0], fin_lean_r: scores[1][1],
+		 STI: data[2].score, STI_lean: desc[2], STI_lean_l: scores[2][0], STI_lean_r: scores[2][1],
+		 immigration: data[3].score, immigration_lean: desc[3], immigration_lean_l: scores[3][0], immigration_lean_r: scores[3][1],
+		 foreign_trade: data[4].score, foreign_trade_lean: desc[4], foreign_trade_lean_l: scores[4][0], foreign_trade_lean_r: scores[4][1],
+		 healthcare_score: data[5].score, healthcare_lean: desc[5], healthcare_lean_l: scores[5][0], healthcare_lean_r: scores[5][1],
+		 gun_control_score: data[6].score, gun_control_lean: desc[6], gun_control_lean_l: scores[6][0], gun_control_lean_r: scores[6][1],
+		 taxation_score: data[7].score, taxation_lean: desc[7], taxation_lean_l: scores[7][0], taxation_lean_r: scores[7][1],
+		 labor_score: data[8].score, labor_lean: desc[8], labor_lean_l: scores[8][0], labor_lean_r: scores[8][1],
+		 environment_score: data[9].score, environment_lean: desc[9], environment_lean_l: scores[9][0], environment_lean_r: scores[9][1],
+		}
+	    )
 	}
 	else{
 	    console.log('Invalid search.')
--- static/js/company.js
+++ static/js/company.js
@@ -1,42 +1,62 @@
-(function($) {
-    $.fn.counter = function(options) {
-	var settings = $.extend({
-	    start: 0,
-	    end: 100,
-	    easing: 'swing',
-	    duration: 400,
-	    complete: ''
-	}, options);
+var twoColComp = {
+    init: function() {
+	var tables = document.getElementsByTagName('table');
 
-	var thisElement = $(this);
+	// for each table
+	for (var i = 0; i < tables.length; i++) {
+	    // don't process one that's already been done (has class two-column-comp)
+	    if (new RegExp('(^| )two-column-comp( |$)', 'gi').test(tables[i].className)){
+		return;
+	    }
 
-	$({
-	    count: settings.start
-	}).animate({
-	    count: settings.end
-	}, {
-	    duration: settings.duration,
-	    easing: settings.easing,
-	    step: function() {
-		var mathCount = Math.ceil(this.count);
-		thisElement.text(mathCount);
-	    },
-	    complete: settings.complete
-	});
-    };
+	    //TODO: need to verify cross-browser support of these vars
+	    var h = tables[i].clientHeight,
+		t = tables[i].getBoundingClientRect().top,
+		wT = window.pageYOffset || document.documentElement.scrollTop,
+		wH = window.innerHeight;
 
-}(jQuery));
+	    if (wT + wH > t + h / 2) {
+		this.make(tables[i]);
+	    }
+	}
+    },
 
-$('#score-counter').counter({
-    start: 0,
-    end: 1483,
-    duration: 4000
-});
+    make: function(el) {
+	var rows = el.getElementsByTagName('tr'),
+	    vals = [],
+	    max,
+	    percent;
 
-$(document).ready(function() {
-    $('.score-graph').each(function() {
-	$(this).find('.score-graph-bar').animate({
-	    width: $(this).attr('data-percent')
-	}, 4000);
-    });
-});
+	// for each row in the table, get vals
+	for (var x = 0; x < rows.length; x++) {
+	    var cells = rows[x].getElementsByTagName('td');
+	    for (var y = 1; y < cells.length; y++) {
+		vals.push(parseInt(cells[y].innerHTML, 10));
+	    }
+	}
+
+	max = Math.max.apply(Math, vals);
+	percent = 100 / max;
+
+	//for each row in the table, apply vals
+	for (x = 0; x < rows.length; x++) {
+	    var cells = rows[x].getElementsByTagName('td');
+	    for (var y = 1; y < cells.length; y++) {
+		var currNum = parseInt(cells[y].innerHTML, 10);
+		cells[y].innerHTML = "";
+		cells[y].style.backgroundSize = currNum + "% 100%";
+		cells[y].style.transitionDelay = x / 20 + "s";
+	    }
+	}
+	//add a class so you don't process it a bunch of times
+	el.className = +" two-column-comp"
+    } // end make
+}
+
+window.onload = function() {
+    twoColComp.init();
+}
+
+window.onscroll = function() {
+    twoColComp.init();
+}
